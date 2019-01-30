0f0018420cdd0304404fa88f7cc274daeef1226d
Bryan Sandoval <bmsandoval@gmail.com>
2019-01-30 16:14:10 -0700

    updating code repo deployment script

--- .scripts/younique/repo-code-fix.sh
+++ .scripts/younique/repo-code-fix.sh
@@ -26,13 +26,21 @@ sudo find ${remote_dir} -type d -exec chmod 0750 {} +
 sudo find ${remote_dir} -type f -exec chmod 0640 {} +
 sudo chown -R ${whoami}:www-data ${remote_dir} 
 sudo npm install
-sudo chmod -R 777 api/tmp
+sudo chmod -R 775 api/tmp
+find /var/www/code/cake/app/tmp/cache/models/ -type f -exec rm '{}' \;
+find /var/www/code/cake/app/tmp/cache/persistent/ -type f -exec rm '{}' \;
+find /var/www/code/api/tmp/cache/models/ -type f -exec rm '{}' \;
+find /var/www/code/api/tmp/cache/persistent/ -type f -exec rm '{}' \;
 sudo npm install -g grunt-cli
 if [[ ! -f /usr/bin/node ]]; then ln -s /usr/bin/nodejs /usr/bin/node; fi;
-cd admin && sudo npm install && sudo npm run install-dnd && sudo npm run install-jgrowl && sudo composer install && cd ../
-cd cake/app && sudo npm install && sudo composer install && cd ../../
-cd api && sudo npm install && sudo composer install && cd ../
-cd api/aws3 && sudo composer install && cd ../../
+cd admin && sudo npm install && sudo npm run install-dnd && sudo npm run install-jgrowl && sudo composer install
+cd ../
+cd cake/app && sudo npm install && sudo composer install
+cd ../../
+cd api && sudo npm install && sudo composer install
+cd ../
+cd api/aws3 && sudo composer install
+cd ../../
 chmod +x api/Console/cake
 sudo api/Console/cake phinx migrate
 EOF
